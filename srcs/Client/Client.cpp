34913a86d46b3d7d092237356719f48892bd6a03
Manuel SHARIFI <manuel.sharifi@gmail.com>
2023-08-05 17:29:59 +0200

    [UP] banger OP for OPER cmd

--- srcs/Client/Client.cpp
+++ srcs/Client/Client.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Client.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ccheyrou <ccheyrou@student.42.fr>          +#+  +:+       +#+        */
+/*   By: msharifi <msharifi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/17 15:42:20 by msharifi          #+#    #+#             */
-/*   Updated: 2023/07/25 16:01:49 by ccheyrou         ###   ########.fr       */
+/*   Updated: 2023/08/05 17:11:49 by msharifi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ Client::Client(void) : _socketFd(-1), _nickName(""), _status(ACCEPT), _channels(
 	return ;
 }
 
-Client::Client(int socketFd, std::string nickname) : _socketFd(socketFd), _nickName(nickname), _status(ACCEPT), _channels(0)
+Client::Client(int socketFd, std::string nickname) : _socketFd(socketFd), _nickName(nickname), _banger(nickname), _status(ACCEPT), _channels(0)
 {
 	_fds_clt.fd = socketFd;
 	_fds_clt.events = POLLIN;
--- srcs/Command/Others/Oper.cpp
+++ srcs/Command/Others/Oper.cpp
@@ -6,7 +6,7 @@
 /*   By: msharifi <msharifi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 14:22:26 by msharifi          #+#    #+#             */
-/*   Updated: 2023/07/05 18:00:31 by msharifi         ###   ########.fr       */
+/*   Updated: 2023/08/05 17:28:19 by msharifi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,13 @@ int	Server::cmdOperErrorHandling(std::vector<std::string> args, Client &client)
 	// }
 	if (args.size() < 2 || args[0].empty() == true)
 	{
-		std::string answer = "461 " + client.getNickname() + " OPER :Not enough parameters\r\n"; // ERR_NEEDMOREPARAMS
+		std::string answer = "461 " + client.getBanger() + " OPER :Not enough parameters\r\n"; // ERR_NEEDMOREPARAMS
 		send(client.getSocket(), answer.c_str(), answer.size(), 0);
 		return (1);
 	}
 	else if (args[0] != OPERNAME || args[1] != OPERPASS)
 	{
-		std::string answer = "464 " + client.getNickname() + " :Password incorrect\r\n"; // ERR_PASSWDMISMATCH
+		std::string answer = "464 " + client.getBanger() + " :Password incorrect\r\n"; // ERR_PASSWDMISMATCH
 		send(client.getSocket(), answer.c_str(), answer.size(), 0);
 		return (1);
 	}
@@ -39,9 +39,9 @@ int	Server::cmdOper(std::vector<std::string> args, Client &client)
 {
 	if (cmdOperErrorHandling(args, client))
 		return (1);
-	std::string answer = "381 " + client.getNickname() + " :You are now an IRC operator\r\n"; // RPL_YOUREOPER
+	std::string answer = "381 " + client.getBanger() + " :You are now an IRC operator\r\n"; // RPL_YOUREOPER
 	send(client.getSocket(), answer.c_str(), answer.size(), 0);
-	answer = ":" + client.getNickname() + "@localhost MODE " + client.getNickname() + " +o\r\n"; // RPL_YOUREOPER
+	answer = ":" + client.getBanger() + " MODE " + client.getNickname() + " +o\r\n"; // RPL_YOUREOPER
 	send(client.getSocket(), answer.c_str(), answer.size(), 0);
 	return (0);
 }
